967e9d84a83b4ac2c446d8e8e34590b2cda90e41
Glogang3hunna <88231208+Glogang3hunna@users.noreply.github.com>
2021-08-07 23:13:34 -0500

    Update __resource.lua

--- Fivem-GloGarage/__resource.lua
+++ Fivem-GloGarage/__resource.lua
@@ -2,14 +2,12 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
-	'@trp-core/locale.lua',
 	'locales/en.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
-	'@trp-core/locale.lua',
 	'locales/en.lua',
 	'config.lua',
 	'client/main.lua'
